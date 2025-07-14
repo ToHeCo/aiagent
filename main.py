ac8338e26f92c0908fb6a0851b9ef6923dc33c00
ToHeCo <tornoema@live.dk>
2025-07-14 09:15:53 +0200

    Added functionality to main.py and added calculator functionality

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ def main():
     import os, sys
     from dotenv import load_dotenv
     from google import genai
+    from google.genai import types
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    user_prompt = sys.argv[1]
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)]),]
     try:
-        response = client.models.generate_content(model = 'gemini-2.0-flash-001', contents = sys.argv[1])
+        response = client.models.generate_content(model = 'gemini-2.0-flash-001', contents = messages)
         print(response.text)
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        if sys.argv[2] == "--verbose":
+            print("User prompt:", user_prompt)
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
     except IndexError:
         print("No prompt was provided")
         exit(1)
